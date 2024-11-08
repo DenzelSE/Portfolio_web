5fa25edbc59dc9ad7dc5b1980c84c4ad0a2e4a4a
DenzelSE <t.d.selokela@gmail.com>
2024-11-08 22:42:40 +0200

    gift card meta data updated as testing

--- src/app/data/projects.tsx
+++ src/app/data/projects.tsx
@@ -14,10 +14,10 @@ export const projects = [
       demo: "#"
     },
     {
-      title: "Gift Card",
-      description: "Real-time weather data visualization using external APIs",
-      techStack: ["React", "Redux", "Chart.js", "REST API"],
-      github: "#",
-      demo: "#"
+      title: "Decentralized Gift Card",
+      description: "A blockchain-powered solution integrated with MiniPay, enabling seamless purchase and secure gifting of digital gift cards.",
+      techStack: ["React", "Solidity", "Typescript", "REST API", "Wagmi"],
+      github: "https://github.com/DenzelSE/CeloGiftCard",
+      demo: "celo-gift-card-react-app.vercel.app"
     }
   ];
